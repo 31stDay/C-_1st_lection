16f98b1b251e0f6fb740fd6c60fdabcb5bc8081b
Dinochka <dina.dina-chernyshova@yandex.ru>
2023-04-07 14:58:55 +0300

    Task03 has been fixed

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -1,33 +1,35 @@
-﻿
-Console.WriteLine("Please enter a number from 1 to 7: ");
+﻿Console.WriteLine("Please enter a number from 1 to 7: ");
 int number = Convert.ToInt32(Console.ReadLine());
-
 if (number == 1)
 {
     Console.WriteLine("Monday");
 }
-if (number == 2)
+else if (number == 2)
 {
     Console.WriteLine("Tuesday");
 }
-if (number == 3)
+else if (number == 3)
 {
     Console.WriteLine("Wednesday");
 }
-if (number == 4)
+else if (number == 4)
 {
     Console.WriteLine("Thursday");
 }
-if (number == 5)
+else if (number == 5)
 {
     Console.WriteLine("Friday");
 }
-if (number == 6)
+else if (number == 6)
 {
     Console.WriteLine("Saturday");
 }
-if (number == 7)
+else if (number == 7)
 {
     Console.WriteLine("Sunday");
+}
+else 
+{
+    Console.WriteLine("This day doesn't exist!");
 }
 
