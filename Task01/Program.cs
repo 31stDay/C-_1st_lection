14c975353cb3032c92779f643d3f8d3c929d2ee3
Dinochka <dina.dina-chernyshova@yandex.ru>
2023-04-07 15:11:45 +0300

    Initial commit for Task01 and Task05

---
+++ Task01/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine("Please enter a first number: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please enter a second number 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number2 * number2 == number1)
+{
+    Console.WriteLine("Number one is a square of number two");
+}
+else
+{
+    Console.WriteLine("Number one isn't a square of number two");
+}
