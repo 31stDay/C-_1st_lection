1db35dbde0254f100cec2c0b0c95b78679fdd5b7
Dinochka <dina.dina-chernyshova@yandex.ru>
2023-04-09 21:18:43 +0300

    Initial commit for Task02

---
+++ Task02/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, 
+//какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Please enter the first number: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Please enter the second number: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > numberB)
+    Console.WriteLine($"The first number- {numberA} is greater than the second one- {numberB}");
+else
+Console.WriteLine($"The second number- {numberB} is greater than the first one- {numberA}");
