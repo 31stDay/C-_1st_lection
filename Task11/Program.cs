98d7ac64e1b2d43ddcb4b3dc69947db464c6eaac
Dinochka <dina.dina-chernyshova@yandex.ru>
2023-04-12 21:47:46 +0300

    Some ajustments have been made

---
+++ Task11/Program.cs
@@ -0,0 +1,20 @@
+﻿// 11. Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = new Random().Next(100, 1000);  // last number isn't included
+Console.WriteLine($"Random number from range 100 - 999 -> {number}");
+
+int DeleteMid(int num)
+{
+    int firstDigit = num / 100;
+    int lastDigit = num % 10;
+    int newNumber = (firstDigit * 10) + lastDigit;
+    return newNumber;
+}
+int midNumber = DeleteMid(number);
+
+Console.WriteLine($"two-digit number is {midNumber}");
---
+++ Task12/Program.cs
@@ -0,0 +1,20 @@
+﻿// 12. Напишите программу, которая будет принимать на
+// вход два числа и выводить, является ли первое число
+// кратным второму. Если первое число не кратно
+// второму, то программа выводит остаток от деления.
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно
+int Rest(int num1, int num2)
+{
+
+}
+
+Console.WriteLine("Please enter the first number: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Please enter the second number: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+
+int rest = Rest(number1, number2);
+Console.WriteLine($"{rest}");
