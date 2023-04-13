1ab7070a8a710b49992445538c8d4260cb732fc7
Dinochka <dina.dina-chernyshova@yandex.ru>
2023-04-13 20:11:37 +0300

    Task14 has been amended

--- Task14/Program.cs
+++ Task14/Program.cs
@@ -5,31 +5,13 @@
 // 46 -> нет
 // 161 -> да
 
-Console.Write("Enter your number: ");
-int number = Convert.ToInt32(Console.ReadLine());
-bool result = MultipleTwoDigits(number, 7, 23);
-Console.WriteLine(result ? "Yes" : "No");
-
 bool MultipleTwoDigits(int num, int num1, int num2)
 {
     return num % num1 == 0 && num % num2 == 0;
 }
 
-
-
-// 14.Напишите программу, которая принимает на
-// вход число и проверяет, кратно ли оно
-// одновременно 7 и 23.
-// 14 -> нет
-// 46 -> нет
-// 161 -> да
-Console.Write("Введите число: ");
+Console.Write("Enter your number: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-bool result = MultipleTwoDigits(number,5,10);
-Console.WriteLine(result ? "да" : "нет");
-
-bool MultipleTwoDigits(int num, int num1,int num2)
-{
-   return num % num1==0 && num % num2==0;
-}
+bool result = MultipleTwoDigits(number, 7, 23);
+Console.WriteLine(result ? "Yes" : "No");
