6d2bea1a2af5d1375d2912197813866c0656747f
Dinochka <dina.dina-chernyshova@yandex.ru>
2023-04-13 20:33:16 +0300

    Task15 is done

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -3,3 +3,21 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+
+bool CheckDayOfWeek(int number)
+{
+    return number == 6 || number == 7;
+}
+
+Console.WriteLine("Please enter a number from 1 to 7: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+if (dayNumber >= 1 && dayNumber <= 7)
+{
+    bool result = CheckDayOfWeek(dayNumber);
+    Console.WriteLine(result ? "Yes" : "No");
+}
+else
+{
+    Console.WriteLine($"Your number- {dayNumber} is out of range 1-7");
+}
