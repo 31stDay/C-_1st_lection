b9774b09f7af055607120539b161e88d02c75e90
Dinochka <dina.dina-chernyshova@yandex.ru>
2023-04-13 19:48:04 +0300

    Task13 has been done

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -3,17 +3,24 @@
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
+int FindThirdDigit(int num)
+{
+    while (num >= 1000)
+    {
+        num = num / 10;
+    }
+    int thDig = num % 10;
+    return thDig;
+}
 
 Console.WriteLine("Please enter your number: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-if (number>= 100)
-FindThirdDigit(number);
-else
-Console.WriteLine("Your number contains less than three digits");
-
-int FindThirdDigit(int num)
+if (number >= 100)
 {
-    
+   int thirdDigit = FindThirdDigit(number);
+   Console.WriteLine($"The third digit of your number{number} is {thirdDigit}");
+} 
+else
+    Console.WriteLine("Your number contains less than three digits.");
 
-}
