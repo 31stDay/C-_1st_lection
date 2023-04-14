b0afbddaac548f62d9640dfbaaa3b41b6d3abe09
Dinochka <dina.dina-chernyshova@yandex.ru>
2023-04-14 14:27:39 +0300

    Task20 has been amended

--- Task20/Program.cs
+++ Task20/Program.cs
@@ -5,15 +5,11 @@
 // A (3,6); B (2,1) -> 5,09
 // A (7,-5); B (1,-1) -> 7,21
 
-
-// double d = Math.Round(Math.Sqrt(5), 2);
-// // double d = Math.Sqrt(5);
-
-// // double d1 = 5.09987654;
-// // double d1Round = Math.Round(d1, 2, MidpointRounding.ToZero);
-// // Console.WriteLine(d1Round);
-
-// System.Console.WriteLine(d);
+double Distance(int x1, int y1, int x2, int y2)
+{
+    double dist = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+    return dist;
+}
 
 Console.WriteLine("Please enter the first point coordinates");
 Console.WriteLine("Please enter X: ");
@@ -27,11 +23,6 @@ int xbCoordinate = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Please enter Y: ");
 int ybCoordinate = Convert.ToInt32(Console.ReadLine());
 
-double Distance(int x1, int y1, int x2, int y2)
-{
-    double dist = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
-    return dist;
-}
 
 double distance = Distance(xaCoordinate, yaCoordinate, xbCoordinate, ybCoordinate);
 double distanceRound = Math.Round(distance, 2);
