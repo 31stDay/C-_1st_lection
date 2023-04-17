b23764d31f97363e1d9a628797f04b377903f7bd
Dinochka <dina.dina-chernyshova@yandex.ru>
2023-04-17 19:36:18 +0300

    Saving intermediate results

---
+++ Task18/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 18: Напишите программу, которая по
+// заданному номеру четверти, показывает диапазон
+// возможных координат точек в этой четверти (x и y).
+
+string Quarter(int n)
+{
+    if (n == 1) return "x > 0 && y > 0";
+    if (n == 2) return "x < 0 && y > 0";
+    if (n == 3) return "x < 0 && y < 0";
+    if (n == 4) return "x > 0 && y < 0";
+    return null;
+}
+
+Console.WriteLine("Please enter coordinate quarter number from 1 to 4: ");
+int coordinateQuarter = Convert.ToInt32(Console.ReadLine());
+
+string range = Quarter(coordinateQuarter);
+Console.WriteLine(range == null
+                    ? "Coordinates quarter number is wrong"
+                    : $"The range of possible coordinates of points in the quarter you specified is {range}");
+
+
+
+// Console.WriteLine("Введите номер четверти: ");
+// int numberQuater = Convert.ToInt32(Console.ReadLine());
+
+// string coordinate = Coordinate(numberQuater);
+// Console.WriteLine(coordinate == null ? "неправильный номер четверти" : coordinate); // тернарный оператор
+
+// string Coordinate(int num)
+// {
+//     if(num == 1) return "x > 0, y > 0";
+//     if(num == 2) return "x < 0, y > 0";
+//     if(num == 3) return "x < 0, y < 0";
+//     if(num == 4) return "x > 0, y < 0";
+//     return null;
+// }
