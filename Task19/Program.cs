0e62cc7f84358094fffeb4c19b27b32d47cd0d74
Dinochka <dina.dina-chernyshova@yandex.ru>
2023-04-18 13:03:32 +0300

    Save

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -4,3 +4,20 @@
 // 14212 -> нет
 // 12821 -> да
 // 23432 -> да
+
+
+Console.WriteLine("Please enter five-digit number: ");
+Convert.ToInt32(Console.ReadLine());
+
+int Promt
+
+
+bool CheckTheNumber(int num)
+{
+    if (num >= 10000 && num < 100000)
+    {
+        return true;
+    }
+    Console.WriteLine("Your number contains more or less then 5 digits.");
+    return false;
+}
