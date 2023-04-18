865a892fc08425e8c2b29c218ff0e875014ab112
Dinochka <dina.dina-chernyshova@yandex.ru>
2023-04-18 14:21:39 +0300

    Task28 has been done

--- Task28/Program.cs
+++ Task28/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 28: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+int ProductOfNumbers(int num)
+{
+    int prod = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        checked
+        {
+            prod = prod * i;
+        }
+    }
+    return prod;
+}
+
+Console.WriteLine("Please enter your number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int prodOfNumbers = ProductOfNumbers(number);
+Console.WriteLine($"Sum  of numbers from 1 to {number} = {prodOfNumbers}");
