863a81553079bdca5f3a10c45921a31d1d3b9b30
Dinochka <dina.dina-chernyshova@yandex.ru>
2023-04-25 14:14:18 +0300

    Task42 is  done

--- Task42/Program.cs
+++ Task42/Program.cs
@@ -3,3 +3,21 @@
 // 45 -> 101101
 // 3 -> 11
 // 2 -> 10
+
+Console.Write("Enter a positive natural decimal number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Convert10to2(int num)
+{
+    int result = 0;
+    int i = 1;
+    while (num > 0)
+    {
+        result += (num % 2) * i;
+        num /= 2;
+        i *= 10;
+    }
+    return result;
+}
+int convert10to2 = Convert10to2(number);
+Console.WriteLine(convert10to2);
