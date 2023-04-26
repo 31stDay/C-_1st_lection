91fc08ec48be84492b0f1d239674c4889468c08e
Dinochka <dina.dina-chernyshova@yandex.ru>
2023-04-26 20:45:45 +0300

    Task45 is done

--- Task42/Program.cs
+++ Task42/Program.cs
@@ -4,9 +4,6 @@
 // 3 -> 11
 // 2 -> 10
 
-Console.Write("Enter a positive natural decimal number: ");
-int number = Convert.ToInt32(Console.ReadLine());
-
 int Convert10to2(int num)
 {
     int result = 0;
@@ -19,5 +16,17 @@ int Convert10to2(int num)
     }
     return result;
 }
+
+// int convert10to2int = Convert10to2Int(number);
+// Console.WriteLine(convert10to2int);
+// for (int i = 0; i < convert10to2String.Length; i++)
+// {
+//    Console.Write(convert10to2String[convert10to2String.Length - 1 - i]);
+// }
+
+
+Console.Write("Enter a positive natural decimal number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
 int convert10to2 = Convert10to2(number);
 Console.WriteLine(convert10to2);
--- Task44/Program.cs
+++ Task44/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 44: Не используя рекурсию, выведите первые N чисел
+// Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+// F(0) = 0; F(1) = 1, F(n) = F(n-1)+F(n-2).
+
+int[] Fibonacci(int size)
+{
+   int[] array = new int[size];
+   // array[0] = 0;
+   array[1] = 1;
+   for (int i = 2; i < array.Length; i++)
+   {
+      array[i] = array[i - 1] + array[i - 2];
+   }
+   return array;
+}
+void PrintArray(int[] arr)
+{
+   for (int i = 0; i < arr.Length; i++)
+   {
+      if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+      else Console.Write($"{arr[i]}");
+   }
+}
+
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] fibonacci = Fibonacci(number);
+PrintArray(fibonacci);
