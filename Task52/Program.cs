3c1f0153ca56da0db8ccba8d684060884e6b3472
Dinochka <dina.dina-chernyshova@yandex.ru>
2023-05-05 17:43:34 +0300

    Task52 has been done

--- Task52/Program.cs
+++ Task52/Program.cs
@@ -5,3 +5,68 @@
 // 5 9 2 3
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] CreateMatrix(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],3}");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+double[] ArrayFromAverage(int[,] matrix)
+{
+    double[] array = new double[matrix.GetLength(1)];
+    int k = 0;
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        double sum = 0;
+        int counter = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            sum = sum + matrix[i, j];
+            counter = counter + 1;
+        }
+        array[k] = sum / counter;
+        k = k + 1;
+    }
+    return array;
+}
+
+
+void PrintArray(double[] array)
+{
+    Console.Write("Average of every column: ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{Math.Round(array[i], 1),2}; ");
+        else Console.Write($"{Math.Round(array[i], 1),2}");
+    }
+}
+
+
+int[,] matr = CreateMatrix(3, 4, 1, 10);
+PrintMatrix(matr);
+double[] arr = ArrayFromAverage(matr);
+PrintArray(arr);
+
