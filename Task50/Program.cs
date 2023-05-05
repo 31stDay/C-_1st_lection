e54f54951badab762c5a04cb0cb66965c4b62111
Dinochka <dina.dina-chernyshova@yandex.ru>
2023-05-05 16:17:00 +0300

    Task50 in progress

--- Task50/Program.cs
+++ Task50/Program.cs
@@ -5,3 +5,60 @@
 // 5 9 2 3
 // 8 4 2 4
 // 1, 7 -> такого элемента в массиве нет
+
+
+int[,] CreateMatrix(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],3}");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+void CheckElement(int[,] matrix, int elRow, int elCol)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        if (i == elRow)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                if (j == elCol) Console.WriteLine($"{matrix[i, j]}");
+              
+            }
+        }
+    }
+}
+
+
+Console.WriteLine("Please enter element position in row: ");
+int elementRow = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Please enter element position in column: ");
+int elementColumn = Convert.ToInt32(Console.ReadLine());
+
+int[,] matr = CreateMatrix(3, 4, 1, 10);
+PrintMatrix(matr);
+CheckElement(matr, elementRow, elementColumn);
+
